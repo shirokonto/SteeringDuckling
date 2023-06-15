d434f2668391f587aea71eeb027f915eaaadf050
mcanti1999 <schoeps.anton@gmail.com>
2023-06-15 22:41:42 +0200

    Added flee and wander steering behaviour

--- src/Duckling.gd
+++ src/Duckling.gd
@@ -2,16 +2,16 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-@export var max_speed: = 5000.0  
+@export var max_speed: = 8000.0  
 
 var screen_size # Size of the game window.
-var _velocity = Vector2.ZERO
+var _velocity: = Vector2.ZERO
+var wander_theta: = -PI / 2;
 
 const DISTANCE_THRESHOLD: = 3.0
 
 func _ready():
 	screen_size = get_viewport_rect().size 
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,18 +21,30 @@ func _physics_process(delta):
 	if global_position.distance_to(coursor_position) < DISTANCE_THRESHOLD:
 		return
 		
-	_velocity = Steering.follow(
+#	_velocity = Steering.flee(
+#		_velocity,
+#		global_position,
+#		coursor_position,
+#		max_speed,
+#		20.0
+#	)
+	
+	_velocity = Steering.wander(
 		_velocity,
 		global_position,
-		coursor_position,
-		max_speed
-	)
+		50.0,		# wander radius
+		wander_theta,
+		max_speed,
+		20.0
+	) 
+	
 	velocity = _velocity * delta
-
-	if velocity.length() > 0:
+	if velocity.length() > 1:
 		sprite.play("walking")
-	else:
-		sprite.play("idle")
+	elif velocity.length() < 1:
+		# print("it should have stopped >:(")
+		sprite.stop()
+		sprite.animation = "idle"
 	
 	move_and_slide()
 	sprite.rotation = _velocity.angle() + (0.5 * PI)
--- src/autoload/Steering.gd
+++ src/autoload/Steering.gd
@@ -2,6 +2,8 @@ extends Node
 
 const DEFAULT_MASS: = 2.0
 const DEFAULT_MAX_SPEED: = 2.0
+const DEFAULT_WANDER_RADIUS: = 50.0
+const DEFAULT_WANDER_THETA: = PI / 2
 
 static func follow(
 		velocity: Vector2,
@@ -13,3 +15,31 @@ static func follow(
 	var desired_velocity: = (target_position - global_position).normalized() * max_speed
 	var steering: = ((desired_velocity - velocity) / mass)
 	return velocity + steering 
+
+static func flee(
+		velocity: Vector2,
+		global_position: Vector2,
+		target_position: Vector2,
+		max_speed: = DEFAULT_MAX_SPEED,
+		mass: = DEFAULT_MASS
+	) -> Vector2:
+	var desired_velocity: = (global_position - target_position).normalized() * max_speed
+	var steering: = ((desired_velocity - velocity) / mass)
+	return velocity + steering 
+	
+static func wander(
+		velocity: Vector2,
+		global_position: Vector2,
+		wander_radius: = DEFAULT_WANDER_RADIUS,
+		wander_theta = DEFAULT_WANDER_THETA,
+		max_speed: = DEFAULT_MAX_SPEED,
+		mass: = DEFAULT_MASS
+	) -> Vector2:
+	var wander_point = velocity.normalized() * 100 + global_position 
+	var theta = wander_theta + velocity.angle()
+	var x = wander_radius * cos(wander_theta)
+	var y = wander_radius * sin(wander_theta)
+	wander_point = wander_point + Vector2(x, y)
+	var steering = ((wander_point - global_position).normalized() * max_speed) / mass
+	return steering
+	
