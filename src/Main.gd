64c4a03372c36a455f379f32929e65600fb88317
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2023-06-17 16:19:23 +0200

    Rename enemy to predator

--- src/Main.gd
+++ src/Main.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-@export var raptor_scene: PackedScene
+@export var predator_scene: PackedScene
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$RaptorTimer.start()
+	$PredatorTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,21 +14,21 @@ func _process(delta):
 
 
 func _on_raptor_timer_timeout():
-	var raptor = raptor_scene.instantiate()
+	var predator = predator_scene.instantiate()
 	
-	var raptor_spawn_location = get_node("RaptorPath/RaptorSpawnLocation")
-	raptor_spawn_location.progress_ratio = randf()
+	var predator_spawn_location = get_node("PredatorPath/PredatorSpawnLocation")
+	predator_spawn_location.progress_ratio = randf()
 	
-	var direction = raptor_spawn_location.rotation + PI / 2
+	var direction = predator_spawn_location.rotation + PI / 2
 	
-	raptor.position = raptor_spawn_location.position
+	predator.position = predator_spawn_location.position
 
 	direction += randf_range(-PI / 4, PI / 4)
-	raptor.rotation = direction
+	predator.rotation = direction
 
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	raptor.linear_velocity = velocity.rotated(direction)
+	predator.linear_velocity = velocity.rotated(direction)
 
-	add_child(raptor)
+	add_child(predator)
 
 	
